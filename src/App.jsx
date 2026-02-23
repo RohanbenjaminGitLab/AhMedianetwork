ff0f096c7787061358edf47a7b87a2c336810bc8
rohan <samothiyajayam@gmail.com>
2026-02-23 08:31:55 +0530

    Update Finish

--- src/App.jsx
+++ src/App.jsx
@@ -38,51 +38,65 @@ function App() {
 
         {/* Social Buttons */}
         <div className="flex flex-col gap-3 sm:gap-4 mt-4 sm:mt-6 w-full items-center">
+
+          {/* Facebook Sports */}
           <SocialButton
             icon={<FaFacebookF />}
             text="FACEBOOK | AH SPORTS"
-            link="https://www.facebook.com/actionhivestreamz"
+            link="fb://facewebmodal/f?href=https://www.facebook.com/actionhivestreamz"
           />
+
+          {/* Facebook Media */}
           <SocialButton
             icon={<FaFacebookF />}
             text="FACEBOOK | AH MEDIA NETWORK"
-            link="https://www.facebook.com/ah.media.network/"
+            link="fb://facewebmodal/f?href=https://www.facebook.com/ah.media.network/"
           />
+
+          {/* YouTube */}
           <SocialButton
             icon={<FaYoutube />}
             text="YOUTUBE | AH MEDIA NETWORK"
-            link="https://youtube.com/@ah_media_network?si=k0m007jo8hkRJvQT"
+            link="vnd.youtube://https://youtube.com/@ah_media_network"
           />
+
+          {/* TikTok */}
           <SocialButton
             icon={<FaTiktok />}
             text="TIKTOK | AH MEDIA NETWORK"
-            link="https://www.tiktok.com/@AHMediaNetwork"
+            link="snssdk1128://user/profile/AHMediaNetwork"
           />
+
+          {/* Instagram */}
           <SocialButton
             icon={<FaInstagram />}
             text="INSTAGRAM | AH MEDIA NETWORK"
-            link="https://www.instagram.com/AHMediaNetwork"
+            link="instagram://user?username=AHMediaNetwork"
           />
+
+          {/* WhatsApp Group */}
           <SocialButton
             icon={<FaWhatsapp />}
             text="WHATSAPP | AH MEDIA NETWORK"
-            link="https://chat.whatsapp.com/CVqeiYIJ4GQ40RExIUbYpV?mode=gi_t"
+            link="https://chat.whatsapp.com/CVqeiYIJ4GQ40RExIUbYpV"
           />
-        </div>
-{/* Developer Credit */}
-<p className="mt-8 text-xs sm:text-sm text-center opacity-70">
-  Developed by <span className="font-semibold"> -Rohanbenjamin</span><br />
 
-  Contact Number{" "}
-  <a
-    href="https://wa.me/94755391504"
-    target="_blank"
-    rel="noopener noreferrer"
-    className="font-semibold underline hover:text-green-400 transition"
-  >
-    -0755391504
-  </a>
-</p>
+        </div>
+
+        {/* Developer Credit */}
+        <p className="mt-8 text-xs sm:text-sm text-center opacity-70">
+          Developed by <span className="font-semibold"> -Rohanbenjamin</span><br />
+
+          Contact Number{" "}
+          <a
+            href="https://wa.me/94755391504"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="font-semibold underline hover:text-green-400 transition"
+          >
+            -0755391504
+          </a>
+        </p>
 
       </div>
     </div>
